48386b92b743616d7fea858c3b659978877dfe7d
AlexandraMoga <amoga@mozilla.com>
2020-11-16 12:40:16 +0200

    Add more tests

--- test_login.py
+++ test_login.py
@@ -3,7 +3,7 @@ import pytest
 from pages.desktop.home import Home
 
 
-
+@pytest.mark.skip
 @pytest.mark.allow_external_http_requests
 def test_login(base_url, selenium, fxa_account):
     """User can login"""
@@ -13,6 +13,7 @@ def test_login(base_url, selenium, fxa_account):
     assert page.logged_in
 
 
+@pytest.mark.skip
 @pytest.mark.allow_external_http_requests
 def test_logout(base_url, selenium, user):
     """User can logout"""
